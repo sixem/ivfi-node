e66286208b641b0f238141b2c1fc0dda9cdc1d48
sixem <emy@five.sh>
2022-12-13 16:38:37 +0100

    Add `transpile.ts` for packaging the project

---
+++ transpile.ts
@@ -0,0 +1,39 @@
+import fs from 'fs-extra';
+import path from 'path';
+import chalk from 'chalk';
+
+import {
+    execSync
+} from 'child_process';
+
+/** Clean, compile and transpile project */
+['clean', 'compile:production', 'transpile:tsc'].forEach((command) =>
+{
+    execSync(`npm run ${command}`, {
+        stdio: 'inherit',
+        cwd: __dirname
+    });
+});
+
+/** Compiled directory */
+const compileDirectory = path.join(__dirname, 'packed');
+
+/** Prepare required package directories */
+['views', 'dist'].forEach((directory) =>
+{
+    fs.copySync(path.join(__dirname, directory), path.join(compileDirectory, directory));
+});
+
+/** Prepare required package files */
+['package.json', '.npmignore'].forEach((file) =>
+{
+    fs.copySync(path.join(__dirname, file), path.join(compileDirectory, file));
+});
+
+/** Pack source */
+execSync(`npm pack`, {
+    stdio: 'inherit',
+    cwd: compileDirectory
+});
+
+console.log(chalk.yellow(`\n\nAttempted to create pack in: ${compileDirectory}\n\nExiting.`));
